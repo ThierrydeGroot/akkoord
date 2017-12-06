e655686b73b19b99e5974a9f182f7094eba05819
Ayy L. Mao <degrootthierry@gmail.com>
2017-12-07 00:44:38 +0100

    simpele batterij niveau tester

---
+++ batterylevel.ino
@@ -0,0 +1,14 @@
+#define BAT_PIN A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  float voltage = (sensorValue * (5.0 / 1024.0) * 10);
+  Serial.println(voltage);
+  delay(1000);
+}
