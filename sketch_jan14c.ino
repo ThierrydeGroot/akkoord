06290616ad29395452181679ca91f70a9b519749
Ayy L. Mao <degrootthierry@gmail.com>
2018-01-14 22:00:39 +0100

    LOGANG 4 LYF

--- sketch_jan14c.ino
+++ sketch_jan14c.ino
@@ -7,8 +7,8 @@
 #define MAX_DISTANCE    250 
 
 int sensorValue = 0; // variable to store the value coming from the sensor
-NewPing sonar(TRIGGER_PIN0, ECHO_PIN0, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
+NewPing sonar0(TRIGGER_PIN0, ECHO_PIN0, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   Serial.begin(115200); //9600
@@ -17,8 +17,7 @@ void setup() {
 void loop() {
   delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 
-  Serial.print(sonar.ping_cm(), DEC); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.print(sonar0.ping_cm(), DEC); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.print("\t");
   Serial.println(sonar1.ping_cm(), DEC); // Secondary ping.
-
 } 
