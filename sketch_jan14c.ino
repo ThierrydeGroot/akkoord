f3f15ae2c017904550abcc013e2be67fe5a56b90
Ayy L. Mao <degrootthierry@gmail.com>
2018-01-14 21:49:01 +0100

    2 ultrasonic sensors sketch.

---
+++ sketch_jan14c.ino
@@ -0,0 +1,24 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN0    12
+#define ECHO_PIN0       11
+#define TRIGGER_PIN1    5
+#define ECHO_PIN1       6
+#define MAX_DISTANCE    250 
+
+int sensorValue = 0; // variable to store the value coming from the sensor
+NewPing sonar(TRIGGER_PIN0, ECHO_PIN0, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
+
+void setup() {
+  Serial.begin(115200); //9600
+}
+
+void loop() {
+  delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+  Serial.print(sonar.ping_cm(), DEC); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.print("\t");
+  Serial.println(sonar1.ping_cm(), DEC); // Secondary ping.
+
+} 
